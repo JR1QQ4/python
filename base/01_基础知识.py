274fbccb7d33c81559b63fe653069bab6cd9eb96
dongqunren <chenjunrenyx@163.com>
2020-11-13 18:55:15 +0800

    complete part1

--- base/01_基础知识.py
+++ base/01_基础知识.py
@@ -31,9 +31,10 @@ add_num = 2 + 3
 sub_num = 3 - 2
 mul_num = 2 * 3
 div_num = 3 / 2
+mod_num = 4 % 3
 exp_num = 3 ** 2
 float_num = 0.1 + 0.2
-print(add_num, sub_num, mul_num, div_num, exp_num, float_num)
+print(add_num, sub_num, mul_num, div_num, mod_num, exp_num, float_num)
 age = 23
 message = "Happy " + str(age) + "rd Birthday!"
 print(message)
@@ -233,58 +234,139 @@ for alien in aliens[:5]:
 print("...")
 print("Total number of aliens: " + str(len(aliens)))
 
+# 五.用户输入和while循环
+print("\n*************** 五.input与while ***************\n")
 
+print("\n======= input =======\n")
+# message = input("Tell me something, and I will repeat it back to you:")
+# print(message)
 
+print("\n======= while =======\n")
+current_number = 1
+while current_number <= 5:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    print(current_number)
 
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+print("\nThe following users have confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user)
 
+# 六.函数
+print("\n***************** 六.函数 ****************\n")
+def greet_user(username):
+    """显示简单的问候语"""
+    print("Hello, " + username.title() + "!")
+greet_user('jesse')
 
+def describe_pet(pet_name, animal_type='dog'):
+    """显示宠物的信息"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+# describe_pet("hamster", "harry")
+# describe_pet("dog","willie")
+describe_pet(pet_name='harry', animal_type="hamster")
 
+def get_formatted_name(first_name, last_name):
+    """返回整洁的姓名"""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
 
+def make_pizza(size, *toppings):
+    """打印顾客点的所有配料"""
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print('- ' + topping)
+make_pizza(16, 'popperoni')
+make_pizza(12, "mushrooms", "greenpeppers", "extra cheese")
 
+def build_profile(first, last, **user_info):
+    """创建一个字典，其中包含我们知道的有关用户的一切"""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+user_profile = build_profile('albert', 'einstenin', location='princeton', field='physics')
+print(user_profile)
 
+# 模块
+print('\n======= 模块 =======\n')
+# import module_name
+# from module_name import function_name
+# from module_name import function_name as fn
+# import module_name as mn
+# from module_name impory *
 
+# 七.类
+print("\n*************** 七,类 ***************\n")
 
+class Dog():
+    """一次模拟小狗的简单尝试"""
+    def __init__(self, name, age):
+        """初始化属性name和age"""
+        self.name = name
+        self.age = age
+    def sit(self):
+        """模拟小狗被命令时蹲下"""
+        print(self.name.title() + " is now sitting.")
+    def roll_over(self):
+        """模拟小狗被命令时打滚"""
+        print(self.name.title() + " rolled over!")
+my_dog = Dog('willine', 6)
+print("My dog's name is " + my_dog.name.title() + ".")
+print("My dog is " + str(my_dog.age) + " years old.")
 
+class Car():
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
+        return long_name.title()
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
 
+# 八.文件和异常
+print("\n*************** 八.文件和异常 ***************\n")
 
+try:
+    filename = 'pi_digits.txt'
+    with open(filename) as file_object:
+        lines = file_object.readlines()
+        for line in lines:
+            print(line.rstrip())
+except:
+    print("Read file error!")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+filename = 'programming.txt'
+with open(filename, 'w') as file_object:
+    file_object.write("I lobe programming.")
+    file_object.write("I love creating new games.")
 
 
 
