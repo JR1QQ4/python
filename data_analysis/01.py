c829813665057134f1d657c184e66de81d8f7bac
dongqunren <chenjunrenyx@163.com>
2021-08-09 23:20:34 +0800

    更新文档

--- data_analysis/01.py
+++ data_analysis/01.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+import time
+
 import requests
+
 
 # 1
 # res = requests.get('http://www.baidu.com')
@@ -34,18 +37,45 @@ import requests
 # print(r3.text)
 
 # 4
-kv = {'key1': 'val1', 'key2': 'val2'}
-r1 = requests.request('GET', 'http://httpbin.org/get', params=kv)
-print(r1.url)
-fs = {'file': open('data.xls', 'wb')}
-r2 = requests.request('POST', 'http://httpbin.org/post', files=fs)
-pxs = {'http': 'ip1', 'https': 'ip2'}
-r3 = requests.request('GET', 'http://httpbin.org/get', proxies=pxs)
+# kv = {'key1': 'val1', 'key2': 'val2'}
+# r1 = requests.request('GET', 'http://httpbin.org/get', params=kv)
+# print(r1.url)
+# fs = {'file': open('data.xls', 'wb')}
+# r2 = requests.request('POST', 'http://httpbin.org/post', files=fs)
+# pxs = {'http': 'ip1', 'https': 'ip2'}
+# r3 = requests.request('GET', 'http://httpbin.org/get', proxies=pxs)
 
+# 5
+# def getHTMLText(url):
+#     try:
+#         r = requests.get('https://www.icourse163.org/', time=30)
+#         r.raise_for_status()
+#         r.encoding = r.apparent_encoding
+#         return r.text
+#     except:
+#         return "产生异常"
+# if __name__ == "__main__":
+#     url = "http://www.httpbin.org/"
+#     totaltime = 0
+#     for i in range(100):
+#         # starttime = time.perf_counter()
+#         starttime = time.time()
+#         getHTMLText(url)
+#         # endtime = time.perf_counter()
+#         endtime = time.time()
+#         totaltime = totaltime + endtime - starttime
+#     print("TotalTime:", totaltime)
 
-
-
-
+# 6
+url = 'https://item.jd.com/100009691096.html'
+try:
+    kv = {'user-agent': 'Mozilla/5.0'}
+    r = requests.get(url, headers=kv)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print(r.text[:1000])
+except:
+    print('爬取失败')
 
 
 
