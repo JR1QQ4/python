0aa7db6ab9cf0db0d969e0d5b766f45d655cab62
dongqunren <chenjunrenyx@163.com>
2021-08-19 23:57:05 +0800

    学习ndarray

---
+++ data_analysis/02.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import numpy as np
+
+
+# def pySum1():
+#     a = [0, 1, 2, 3, 4]
+#     b = [9, 8, 7, 6, 5]
+#     c = []
+#     for i in range(len(a)):
+#         c.append(a[i]**2 + b[i]**3)
+#     print(c)
+# def pySum2():
+#     a = np.array([0, 1, 2, 3, 4])
+#     b = np.array([9, 8, 7, 6, 5])
+#     c = a**2 + b**3
+#     print(c)
+# pySum1()
+# pySum2()
+
+print(np.arange(10))
+print(np.ones((3, 6)))
+print(np.zeros((3, 6), dtype=np.int32))
+print(np.eye(5))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
