78d91a9bbb29af748e07c5946ab0c207a536097c
dongqunren <chenjunrenyx@163.com>
2021-08-21 23:26:50 +0800

    ndarray操作

--- data_analysis/02.py
+++ data_analysis/02.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 
+# 1
 # def pySum1():
 #     a = [0, 1, 2, 3, 4]
 #     b = [9, 8, 7, 6, 5]
@@ -18,13 +19,27 @@ import numpy as np
 # pySum1()
 # pySum2()
 
-print(np.arange(10))
-print(np.ones((3, 6)))
-print(np.zeros((3, 6), dtype=np.int32))
-print(np.eye(5))
+# 2
+# print(np.arange(10))
+# print(np.ones((3, 6)))
+# print(np.zeros((3, 6), dtype=np.int32))
+# print(np.eye(5))
 
+# 3
+a = np.array([9, 8, 7, 6, 5])
+print(a[2])  # 7
+print(a[1:4:2])  # [8 6]，[起始编号：终止编号（不含）：步长]
 
+a1 = np.arange(24).reshape((2, 3, 4))
+print(a1)
+print(a1[1, 2, 3])  # 23
+print(a1[0, 1, 2])  # 6
+print(a1[-1, -2, -3])  # 17
 
+a2 = np.arange(24).reshape((2, 3, 4))
+print(a2[:, 1, -3])
+print(a2[:, 1:3, :])
+print(a2[:, :, ::2])
 
 
 
