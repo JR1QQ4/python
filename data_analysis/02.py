0c2b46f66debd16a18e1c50b3906dbd926cac748
dongqunren <chenjunrenyx@163.com>
2021-08-24 21:52:30 +0800

    引力波

--- data_analysis/02.py
+++ data_analysis/02.py
@@ -76,10 +76,48 @@ from PIL import Image
 # 10
 import matplotlib.pyplot as plt
 
-plt.plot([3, 1, 4, 5, 2])
-plt.ylabel("Grade")
+# plt.plot([3, 1, 4, 5, 2])
+# plt.ylabel("Grade")
 # plt.savefig('test', dpi=600)  # PNG文件
-plt.show()
+# plt.show()
+
+# 11、引力波
+from scipy.io import wavfile
+
+# rate_h, hstrain = wavfile.read(r'H1_Strain.wav')
+# rate_l, lstrain = wavfile.read(r'L1_Strain.wav')
+# reftime, ref_H1 = np.genfromtxt('wf_template.txt').transpose()
+# # ------------------------------------------------------------
+# htime_interval = 1/rate_h
+# ltime_inrterval = 1/rate_l
+# htime_len = hstrain.shape[0]/rate_h
+# htime = np.arange(-htime_len/2, htime_len/2, htime_interval)
+# ltime_len = lstrain.shape[0]/rate_l
+# ltime = np.arange(-ltime_len/2, ltime_len/2, ltime_inrterval)
+# # ------------------------------------------------------------
+# fig = plt.figure(figsize=(12, 6))
+# plth = fig.add_subplot(221)
+# plth.plot(htime, hstrain, 'y')
+# plth.set_xlabel('Time (seconds)')
+# plth.set_ylabel('H1 Strain')
+# plth.set_title('H1 train')
+# # ------------------------------------------------------------
+# plt1 = fig.add_subplot(222)
+# plt1.plot(ltime, lstrain, 'g')
+# plt1.set_xlabel('Time (seconds)')
+# plt1.set_ylabel('L1 Strain')
+# plt1.set_title('L1 train')
+# # ------------------------------------------------------------
+# pltref = fig.add_subplot(212)
+# pltref.plot(reftime, ref_H1)
+# pltref.set_xlabel('Time (seconds)')
+# pltref.set_ylabel('Template Strain')
+# pltref.set_title('Template')
+# # ------------------------------------------------------------
+# fig.tight_layout()
+# # plt.savefig("Gravitational_Waves_Original.png")
+# plt.show()
+# plt.close(fig)
 
 
 
