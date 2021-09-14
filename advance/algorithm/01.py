206d532c0bbae1cd6db891ff6ddb1afc9c239257
dongqunren <chenjunrenyx@163.com>
2021-09-14 23:22:56 +0800

    更新代码

--- advance/algorithm/01.py
+++ advance/algorithm/01.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
-low = 0
-high = len(list) - 1
-mid = (low + high) / 2
-guess = list[mid]
-if guess < item:
-    low = mid + 1
+import math
+
+
+# low = 0
+# high = len(list) - 1
+# mid = (low + high) / 2
+# guess = list[mid]
+# if guess < item:
+#     low = mid + 1
+
+
+def binary_search(lst: list, item):
+    low = 0
+    high = len(lst) - 1
+    while low <= high:
+        mid = (low + high) / 2
+        guess = lst[math.floor(mid)]
+        print("mid={}, guess={}, high={}".format(mid, guess, high))
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            high = mid + 1
+    return None
+
+
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, 3))
+print(binary_search(my_list, -1))
