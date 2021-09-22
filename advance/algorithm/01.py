c0ffe9b21cd5a3d1dbca7932b2764fe6843ef2cd
dongqunren <chenjunrenyx@163.com>
2021-09-22 22:29:10 +0800

    ‘g更新代码

--- advance/algorithm/01.py
+++ advance/algorithm/01.py
@@ -36,23 +36,27 @@ import math
 # 删除：数组 O(n)，链表 O(1)
 
 # 4、选择排序
-def findSmallest(arr: list):
-    smallest = arr[0]
-    smallest_index = 0
-    for i in range(1, len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_index = i
-    return smallest_index
-def selectionSort(arr: list):
-    newArr = []
-    for i in range(len(arr)):
-        smallest = findSmallest(arr)
-        newArr.append(arr.pop(smallest))
-    return newArr
-print(selectionSort([5, 3, 6, 2, 10]))
+# def findSmallest(arr: list):
+#     smallest = arr[0]
+#     smallest_index = 0
+#     for i in range(1, len(arr)):
+#         if arr[i] < smallest:
+#             smallest = arr[i]
+#             smallest_index = i
+#     return smallest_index
+# def selectionSort(arr: list):
+#     newArr = []
+#     for i in range(len(arr)):
+#         smallest = findSmallest(arr)
+#         newArr.append(arr.pop(smallest))
+#     return newArr
+# print(selectionSort([5, 3, 6, 2, 10]))
 
-
-
+# 5、递归
+# def fact(x):
+#     if x == 1:
+#         return 1
+#     else:
+#         return x * fact(x - 1)
 
 
