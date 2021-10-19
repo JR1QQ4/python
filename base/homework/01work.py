b4db47c26b9e927f64d489306070d292c608f05e
dongqunren <chenjunrenyx@163.com>
2021-10-19 22:54:29 +0800

    更新代码

--- base/homework/01work.py
+++ base/homework/01work.py
@@ -3,14 +3,31 @@
 # 一、哪些不能作为变量？
 # 字母、数字、下划线，不属于关键字，不以数字开头的字符串
 
+# 二、修改列表【1，2，3，4，5】为【0，1，2，3，66，5，11，22，33】并排序
+# li1 = [1, 2, 3, 4, 5]
+# li1[3] = 66
+# li1.insert(0, 0)
+# li1.extend([11, 22, 33])
+# li1.sort(reverse=True)
+# print(li1)
 
+# 三、修改{}为{'a':'a','b':'b'}，然后添加元素为{'a':'a','b':'b','c':'c','d':'d'}
+# 删除最后一个添加的元素，然后清空
+# dict1 = {}
+# dict1.update({'a': 'a', 'b': 'b'})
+# print(dict1)
+# dict1.update({'c': 'c', 'd': 'd'})
+# print(dict1)
+# dict1.popitem()
+# print(dict1)
+# dict1.pop('c')
+# print(dict1)
+# dict1.clear()
+# print(dict1)
+# dict1['a'] = 'aa'
+# print(dict1['a'], dict1.get('a'))
 
-
-
-
-
-
-
+# 四、('a','a')，('b','b')，lis2=[('c','c')]变为字典{}
 
 
 
