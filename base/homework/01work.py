70b191bb14a8aaa8fa1c0523bdba1d823f0c4c48
dongqunren <chenjunrenyx@163.com>
2021-10-20 23:18:47 +0800

    更新代码

--- base/homework/01work.py
+++ base/homework/01work.py
@@ -27,7 +27,54 @@
 # dict1['a'] = 'aa'
 # print(dict1['a'], dict1.get('a'))
 
-# 四、('a','a')，('b','b')，lis2=[('c','c')]变为字典{}
+# 四、('a','a')，('b','b')，lis2=[('c','c')]变为字典{'a':'a','b':'b','c':'c'}
+# lis2 = [('c', 'c')]
+# lis2.append(('a', 'a'))
+# lis2.append(('b', 'b'))
+# print(dict(lis2))
+
+# 五、实现文字版的剪刀石头布游戏，石头（1）剪刀（2）步（3）退出（4）
+import random
+import sys
+
+while True:
+    c = random.randint(1, 3)
+    a = input("请输入你要出，石头（1）剪刀（2）布（3）退出（4）：")
+    if a == '1':
+        if c == 1:
+            print("平局！")
+        elif c == 2:
+            print("用户胜！")
+        elif c == 3:
+            print("电脑胜！")
+    elif a == '2':
+        if c == 1:
+            print("电脑胜！")
+        elif c == 2:
+            print("平局！")
+        elif c == 3:
+            print("用户胜！")
+    elif a == '3':
+        if c == 1:
+            print("用户胜！")
+        elif c == 2:
+            print("电脑胜！")
+        elif c == 3:
+            print("平局！")
+    elif a == '4':
+        print('游戏退出')
+        break
+    else:
+        print("输入的内容不正确，请重新输入！")
+        continue
+
+
+
+
+
+
+
+
 
 
 
