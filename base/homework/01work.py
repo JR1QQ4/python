54cb403faef1dcd85f5ca54bde14b12c8aa2e6f0
dongqunren <chenjunrenyx@163.com>
2021-10-21 21:17:21 +0800

    更新代码

--- base/homework/01work.py
+++ base/homework/01work.py
@@ -34,46 +34,59 @@
 # print(dict(lis2))
 
 # 五、实现文字版的剪刀石头布游戏，石头（1）剪刀（2）步（3）退出（4）
-import random
-import sys
+# import random
+#
+#
+# li = ['石头', '剪刀', '布']
+# while True:
+#     c = random.randint(1, 3)
+#     a = int(input("请输入你要出，石头（1）剪刀（2）布（3）退出（4）："))
+#     if 1 <= a <= 3:
+#         print("你出的是【{}】，电脑出的是【{}】。".format(li[a-1], li[c-1]), end='')
+#         if (a - c == -1) or (a - c == 2):
+#             print("用户胜！")
+#         elif a == c:
+#             print("平局！")
+#         else:
+#             print("电脑胜！")
+#     elif a == 4:
+#         print("退出！")
+#         break
+#     else:
+#         print("输入的内容不正确，请重新输入！")
+#     # if a == '1':
+#     #     if c == 1:
+#     #         print("平局！")
+#     #     elif c == 2:
+#     #         print("用户胜！")
+#     #     elif c == 3:
+#     #         print("电脑胜！")
+#     # elif a == '2':
+#     #     if c == 1:
+#     #         print("电脑胜！")
+#     #     elif c == 2:
+#     #         print("平局！")
+#     #     elif c == 3:
+#     #         print("用户胜！")
+#     # elif a == '3':
+#     #     if c == 1:
+#     #         print("用户胜！")
+#     #     elif c == 2:
+#     #         print("电脑胜！")
+#     #     elif c == 3:
+#     #         print("平局！")
+#     # elif a == '4':
+#     #     print('游戏退出')
+#     #     break
+#     # else:
+#     #     print("输入的内容不正确，请重新输入！")
+#     #     continue
 
-while True:
-    c = random.randint(1, 3)
-    a = input("请输入你要出，石头（1）剪刀（2）布（3）退出（4）：")
-    if a == '1':
-        if c == 1:
-            print("平局！")
-        elif c == 2:
-            print("用户胜！")
-        elif c == 3:
-            print("电脑胜！")
-    elif a == '2':
-        if c == 1:
-            print("电脑胜！")
-        elif c == 2:
-            print("平局！")
-        elif c == 3:
-            print("用户胜！")
-    elif a == '3':
-        if c == 1:
-            print("用户胜！")
-        elif c == 2:
-            print("电脑胜！")
-        elif c == 3:
-            print("平局！")
-    elif a == '4':
-        print('游戏退出')
-        break
-    else:
-        print("输入的内容不正确，请重新输入！")
-        continue
-
-
-
-
-
-
-
+# 六、九九乘法表
+# for i in range(1, 10):
+#     for j in range(1, i+1):
+#         print("{}*{}={:<4}".format(i, j, i*j), end='')
+#     print()
 
 
 
